9c6e95044059a74cf2079f29adda9ab58155bdb9
Manav Misra <manavm@visionwebsoft.com>
2022-01-27 08:34:20 -0600

    Become RESTful by adding links along with the resources

--- src/main/java/tech/codefinity/employee/EmployeeController.java
+++ src/main/java/tech/codefinity/employee/EmployeeController.java
@@ -1,8 +1,14 @@
 package tech.codefinity.employee;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController // No stupid templates - just send response body with the data
 class EmployeeController {
@@ -13,12 +19,29 @@ class EmployeeController {
     this.repository = repository;
   }
 
-  //  TODO: Find out what this is 👇🏾 😖
+  // The Aggregate Root is the parent Entity to all other Entities and Value Objects within the
+  // Aggregate.
+  // https://www.google.com/search?q=What+is+an+aggregate+root
   //    Aggregate root
   //    tag::get-aggregate-root[]
   @GetMapping("/employees")
-  List<Employee> index() {
-    return repository.findAll();
+  CollectionModel<EntityModel<Employee>> index() {
+
+    List<EntityModel<Employee>> employees =
+
+        //            Transform the employees into Entities (include links)
+        repository.findAll().stream()
+            .map(
+                employee ->
+                    EntityModel.of(
+                        employee,
+                        linkTo(methodOn(EmployeeController.class).show(employee.getId()))
+                            .withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).index()).withRel("employees")))
+            .collect(Collectors.toList());
+
+    return CollectionModel.of(
+        employees, linkTo(methodOn(EmployeeController.class).index()).withSelfRel());
   }
   //  end::get-aggregate-root[]
 
@@ -28,9 +51,20 @@ class EmployeeController {
   }
 
   @GetMapping("/employees/{id}")
-  Employee show(@PathVariable Long id) {
+  //  EntityModel<T> is a generic container from Spring HATEOAS that includes not only the data but
+  // a collection of links.
+  //  https://spring.io/guides/tutorials/rest/
+  EntityModel<Employee> show(@PathVariable Long id) {
 
-    return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    Employee foundEmployee =
+        repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+
+    return EntityModel.of(
+        foundEmployee,
+
+        //        Create a 'self link' to the individual employee
+        linkTo(methodOn(EmployeeController.class).show(id)).withSelfRel(),
+        linkTo(methodOn(EmployeeController.class).index()).withRel("employees"));
   }
 
   @PutMapping("/employees/{id}")
