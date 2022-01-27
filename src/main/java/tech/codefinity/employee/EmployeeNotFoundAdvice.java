56db3d7e15bcef35e68dd5453f3a02e36d37d7f6
Manav Misra <manavm@visionwebsoft.com>
2022-01-27 07:48:56 -0600

    :goal_net: Controller Advice for 404 Employee not found Response

---
+++ src/main/java/tech/codefinity/employee/EmployeeNotFoundAdvice.java
@@ -0,0 +1,19 @@
+package tech.codefinity.employee;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// Special @Component specifically for intercepting exceptions stemming from @RequestMapping
+@ControllerAdvice
+class EmployeeNotFoundAdvice {
+
+  @ResponseBody
+  @ExceptionHandler(EmployeeNotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  String handleEmployeeNotFound(EmployeeNotFoundException e) {
+    return e.getMessage();
+  }
+}
